8aca83307ccb1fb967a76f5e28ab728d26144cb5
bargaviumapathy <bargaviumapathy@gmail.com>
2018-12-12 23:53:12 +0530

    Updates

---
+++ precache-manifest.14a776b922810a21146630605c5abb0e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/ReactQuotesToday/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "a30db5e7961f4b4527a7",
+    "url": "/ReactQuotesToday/static/css/main.34c08e36.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/ReactQuotesToday/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "c57ce973bcfd154f5cde",
+    "url": "/ReactQuotesToday/static/js/1.c57ce973.chunk.js"
+  },
+  {
+    "revision": "0fce9bdaad239769b01d",
+    "url": "/ReactQuotesToday/static/js/runtime~main.0fce9bda.js"
+  },
+  {
+    "revision": "a30db5e7961f4b4527a7",
+    "url": "/ReactQuotesToday/static/js/main.a30db5e7.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/ReactQuotesToday/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/ReactQuotesToday/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/ReactQuotesToday/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "c57ce973bcfd154f5cde",
+    "url": "/ReactQuotesToday/static/css/1.0442ec24.chunk.css"
+  },
+  {
+    "revision": "909befb61cbb54a77a262881ba468e40",
+    "url": "/ReactQuotesToday/index.html"
+  }
+];
